68ce851a39a8b87b8f6d3d3fbd3a4be0d11f7087
usmanahmad4823 <usmanahmad0490@gmail.com>
2026-05-15 21:44:07 +0500

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ export const metadata: Metadata = {
     template: `%s — ${siteConfig.name}`,
   },
   description: siteConfig.description,
+  icons: {
+    icon: '/favicon.png',
+  },
   openGraph: {
     title: `${siteConfig.name} — ${siteConfig.tagline}`,
     description: siteConfig.description,
